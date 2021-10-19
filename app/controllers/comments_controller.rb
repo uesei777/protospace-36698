32cc3c777852271ca5d672433f94fcd7460fb56b
uesei777 <sreiibyoa.1227@i.softbank.jp>
2021-10-19 11:34:29 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+  def create
+    if Comment.create(comment_params)
+      redirect_to prototype_path(params[:prototype_id])
+    else
+      render :show
+    end
+    
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+end
